6b27392577c00043f9b4a1084b2f0534df94d4c7
bemixzyy <bolunlun1210@gmail.com>
2026-02-24 10:08:12 +0700

    first commit

---
+++ language-learning-backend/src/main/java/com/languageapp/language_learning_backend/LanguageLearningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.languageapp.language_learning_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearningBackendApplication.class, args);
+	}
+
+}
---
+++ language-learning-backend/src/test/java/com/languageapp/language_learning_backend/LanguageLearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.languageapp.language_learning_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
