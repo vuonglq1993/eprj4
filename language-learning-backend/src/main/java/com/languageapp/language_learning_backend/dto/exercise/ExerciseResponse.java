fbc579149a5cc51b4feb02e2354c17ea74e067e1
nguyenhuyhoang <nguyenhuyhoang19102005hh@gmail.com>
2026-03-21 01:48:31 +0700

    exercise api + swagger ui

---
+++ language-learning-backend/src/main/java/com/languageapp/language_learning_backend/dto/exercise/ExerciseResponse.java
@@ -0,0 +1,17 @@
+package com.languageapp.language_learning_backend.dto.exercise;
+
+import com.languageapp.language_learning_backend.entity.Exercise.ExerciseType;
+import lombok.*;
+import java.util.UUID;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ExerciseResponse {
+    private UUID         id;
+    private UUID         lessonId;
+    private String       title;
+    private ExerciseType type;
+    private String       questionData;
+    private Integer      orderIndex;
+    private Integer      points;
+    private Integer      timeLimitSeconds;
+}
